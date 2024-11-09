3a85994c7e0a00a4aa4bf4b6927b57b89ac68395
Lucas Simon <lucasimonmendes@hotmail.com>
2024-11-09 11:24:49 -0300

    :sparkles: (server): add a route /docs

--- backend/server/src/app.controller.ts
+++ backend/server/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('docs')
+  @Redirect('https://notes.s1m0n.tech', 302)
+  getDocs(@Query('version') version) {
+    if (version && version === '5') {
+      return { url: 'https://blog.s1m0n.tech' };
+    }
   }
 }
